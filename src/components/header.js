fff2eb31bb0603e7da9a86d9dad26e8ce3dfcaea
crstern <stern.cristi@gmail.com>
2020-04-14 10:30:36 +0300

    navbar added but not final

--- src/components/header.js
+++ src/components/header.js
@@ -3,11 +3,13 @@ import './header.css'
 
 function Header({items}){
     return (
-        <header>
-            <h1>PostIT</h1>
-            <div className="options">
+        <header className="navbar navbar-light">
+            <nav className="navbar navbar-light">
+                <span class="navbar-brand mb-0 h1" style={{background : "transparent"}}>PostIT</span>
+            </nav>
+            <div className="nav justify-content-end">
                 {items.map(item => 
-                    <div>{item.option}</div>
+                    <a class="nav-link active" href="#">{item.option}</a> 
                 )}
             </div>
         </header>
